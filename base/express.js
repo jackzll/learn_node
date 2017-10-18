85406c7408dd1a0933fcca3d03b3b8c649ec2712
jackzll <2256840980@qq.com>
2017-10-18 23:21:21 +0800

    体验express

--- base/express.js
+++ base/express.js
@@ -1,11 +1,29 @@
 var express=require('express');
 var path=require('path');
 var app=express();
-console.log(path.join(__dirname,'/name.txt'));
+var bodyParser=require('body-parser');
+let rootDir=__dirname;
+app.use(bodyParser.urlencoded({
+	extended:false
+}))
+//配置首页
+let indexHTML=path.join(rootDir,'../web/index.html')
+// 配置虚拟目录和静态资源文件夹
+app.use('/static',express.static(path.join(rootDir,'../web/')))
+// app.use(express.static(path.join(rootDir,'../web/')))
+
+// 访问路由
 app.get('/',function(req,res){
-	path.join(__dirname,'/name.txt');
-	res.sendFile(path.join(__dirname,'/a.html'));
+	res.sendFile(indexHTML);
 	console.log(req.query);
+})
+app.post('/userInfo',function(req,res){
+	console.log(req.body);
+	const{username}=req.body;
+	res.json({
+		status:"success",
+		name:username
+	});
 })
 var server=app.listen(8081,function(){
 	var host=server.address().address
